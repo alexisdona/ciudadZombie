55198a6fdbfe69116de81fa3a9407c917ecb2e59
Alexis Do Nascimento <alexis.dona@gmail.com>
2020-01-26 23:32:34 -0300

    jugador se mueve

--- js/jugador.js
+++ js/jugador.js
@@ -1,16 +1,29 @@
 /* El objeto jugador es un objeto literal que se encuentra incompleto.
  Solo tiene asignadas algunas de sus propiedades y ningun metodo */
 var Jugador = {
-  /* el sprite contiene la ruta de la imagen
-  */
-  sprite: 'imagenes/auto_rojo_abajo.png',
-  x: 130,
-  y: 160,
-  ancho: 15,
-  alto: 30,
-  velocidad: 10,
-  vidas: 5,
-  // Hay que agregar lo que falte al jugador: movimientos, perdida de vidas,
-  // y todo lo que haga falta para que cumpla con sus responsabilidades
+    /* el sprite contiene la ruta de la imagen
+     */
+    sprite: 'imagenes/auto_rojo_abajo.png',
+    x: 130,
+    y: 160,
+    ancho: 15,
+    alto: 30,
+    velocidad: 10,
+    vidas: 5,
+    quitarVidas: function(cantVidas) {
+        this.vidas -= cantVidas;
+    },
+    mover: function(posX, posY, pSprite) {
+        this.x += posX;
+        this.y += posY;
+    },
+    deCostado: function() {
+        this.ancho = 30;
+        this.alto = 15;
+    },
+    parado: function() {
+        this.ancho = 15
+        this.alto = 30;
+    }
 
 }
